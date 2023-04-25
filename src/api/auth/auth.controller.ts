e674390298d8e2b43903c584074a04cdc0bc3662
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-04-25 14:56:52 +0100

    fix: Fix an error in the auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -10,6 +10,7 @@ const accessTokenCookieOptions: CookieOptions = {
   maxAge: 900000, // 15mins
   httpOnly: true,
   path: '/',
+  domain: 'ideafundr.onrender.com',
   sameSite: 'none',
   secure: NODE_ENV === 'production'
 };
@@ -18,6 +19,7 @@ const refreshTokenCookieOptions: CookieOptions = {
   maxAge: 3.154e10, // 1 year
   httpOnly: true,
   path: '/',
+  domain: 'ideafundr.onrender.com',
   sameSite: 'none',
   secure: NODE_ENV === 'production'
 };
